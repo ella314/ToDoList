8c032772fedd3eceb3baad603e3d9562836bb869
Ella Ragoler <ella.ragoler@gmail.com>
2025-07-25 13:47:06 -0700

    Added Swift Data

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    
     var body: some View {
         VStack {
             HStack{
@@ -28,9 +31,14 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+            List {
+                    ForEach (toDos) { toDoItem in
+                            Text(toDoItem.title)
+                    }
+            }
         }
         if showNewTask {
-            NewToDoView()
+            NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false))
         }
     }
 }
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -6,20 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
             Text("Task Title:")
                 .font(.title2)
                 .fontWeight(.semibold)
-            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
                 .font(.title3)
                 .padding()
                 .background(Color(.systemGroupedBackground))
                 .cornerRadius(15)
                 .padding()
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            Toggle(isOn: $toDoItem.isImportant) {
                 Text("Is it important?")
                     .font(.title3)
             }
@@ -38,5 +41,5 @@ struct NewToDoView: View {
 }
 
 #Preview {
-    NewToDoView()
+    NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false))
 }
